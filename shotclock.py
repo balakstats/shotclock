6f25d0a46c70a2d568b8fc65dbbd4351b3c15697
balakstats <toth-balazs@outlook.com>
2019-11-30 19:38:17 +0000

    first try

---
+++ shotclock.py
@@ -0,0 +1,8 @@
+from samplebase import SampleBase
+
+class SimpleSquare(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(SimpleSquare, self).__init__(*args, **kwargs)
+
+    def run(self):
+#        
