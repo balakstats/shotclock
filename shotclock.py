1d0fcef41fb0a5dfb61bcc35d72b569fe02aafe6
balakstats <toth-balazs@outlook.com>
2020-01-24 20:01:04 +0000

    gut

--- shotclock.py
+++ shotclock.py
@@ -18,7 +18,6 @@ class RunText(SampleBase):
         device_id = "hci0"
         status, output = subprocess.getstatusoutput(cmd)
         bt_mac = output.split("{}:".format(device_id))[1].split("BD Address: ")[1].split(" ")[0].strip()
-#        hostMACAddress = "B8:27:EB:25:33:99"
         hostMACAddress = bt_mac
         print(bt_mac)
         port = 1
@@ -26,57 +25,51 @@ class RunText(SampleBase):
         size = 1024
         s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
         s.bind((hostMACAddress,port))
-        print("here")
         s.listen(backlog)
+        print("Wait for bluetooth connection")
         client, address = s.accept()
         print("bluetooth connected")
 
         offset_canvas = self.matrix.CreateFrameCanvas()
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
-#        font.LoadFont("/home/pi/Zeitnehmung/fonts/shotclockNumbers.bdf")
-        font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/7x14.bdf")
-        textColor = graphics.Color(255,255,0)
+        font.LoadFont("/home/pi/Zeitnehmung/fonts/shotclockFonts/shotclockNumbers.bdf")
+        textColorGreen = graphics.Color(255,255,0) # green
+        textColorRed   = graphics.Color(255,0,0) # red
+
         i = 0
-
         while True:
-            data = client.recv(size)
-           # for i in range(32):
-            #    for j in range(32):
-           # offset_canvas.Clear()
-           # offscreen_canvas.Clear()
-#            offset_canvas.Clear()
-            offset_canvas.SetPixel(0,0,255,0,255)
-#            offset_canvas.SetPixel(0,15,255,0,255)
-#            offset_canvas.SetPixel(20,20,255,0,255)
-#            offset_canvas.SetPixel(31,31,255,0,255)
-               #     time.sleep(0.1)
-#                    offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
-#            text = ["00","11","22","33","44","55","66","77","88","99"]
- #           i = i+1
-#            if i == 1:
-            if data:
-                print(str(data))
-                text = str(data).split("'")[1].strip("'")
-                print(text)
-                offscreen_canvas.Clear()
-                graphics.DrawText(offscreen_canvas, font, 0, 31, textColor, text)
-                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-           # offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
+            try:
+                data = client.recv(size)
+            except Exception as ex:
+                print("Lost bluetooth connection")
+                print(ex)
+                s.close()
+                time.sleep(0.5)
+                s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+                s.bind((hostMACAddress,port))
+                s.listen(backlog)
+                client, address = s.accept()
+#            offset_canvas.SetPixel(0,0,255,0,255)
 
-#            time.sleep(0.5)
-
+            try:
+                if data:
+                    print(str(data))
+                    text = str(data).split("'")[1].strip("'")
+                    print(text)
+                    offscreen_canvas.Clear()
+                    graphics.DrawText(offscreen_canvas, font, 0, 31, textColorGreen if int(text)>5 else textColorRed, text)
+                    offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+            except Exceptiopn as ex:
+                print("could not process message")
+                print(ex)
+            time.sleep(0.3)
 
 if __name__=="__main__":
   #  simple_square=SimpleSquare()
 #    if(not simple_square.process()):
  #       simple_square.print_help()
-    while True:
-        try:
-            print("Start shotclock")
-            run_text = RunText()
-            if (not run_text.process()):
-                run_text.print_help()
-        except:
-            print("Lost connection")
-            time.sleep(5)
+    print("Start shotclock")
+    run_text = RunText()
+    if (not run_text.process()):
+        run_text.print_help()
