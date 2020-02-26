d280cb94883062ac79a672a651bcb3b4560b1137
balakstats <toth-balazs@outlook.com>
2020-02-26 22:21:48 +0000

    startup_3

---
+++ startup_3.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sleep 10
+sudo python3 /home/pi/Zeitnehmung/shotclock.py --led-brightness=10 --led-rows=16 --led-cols=32 --led-multiplexing=4 --led-pixel-mapper=rotate:90 --led-parallel=3
