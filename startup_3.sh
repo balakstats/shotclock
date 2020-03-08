1ea5d85186e9aee6209e0037800c5c2524f873da
balakstats <toth-balazs@outlook.com>
2020-03-08 12:01:32 +0100

    Update startup_3.sh
    go

--- startup_3.sh
+++ startup_3.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sleep 10
-sudo python3 /home/pi/Zeitnehmung/shotclock.py --led-brightness=10 --led-rows=16 --led-cols=32 --led-multiplexing=4 --led-parallel=3
+sudo python3 /home/pi/shotclock/shotclock.py --led-brightness=10 --led-rows=16 --led-cols=32 --led-multiplexing=4 --led-parallel=3
